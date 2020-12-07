875654960ab356911a23131d2626edc518f49a0b
Dinuka De Silva <l.dinukadesilva@gmail.com>
2020-12-07 20:28:06 +0530

    Adding users and groups management components to EMC settings page

--- custos-demo-gateway/src-emc/pages/Settings.vue
+++ custos-demo-gateway/src-emc/pages/Settings.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>Settings</div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h2>Settings</h2>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <Users/>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <Groups/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import Users from "@/components/workspace/Users";
+import Groups from "@/components/workspace/Groups";
+
 export default {
-  name: "Settings"
+  name: "Settings",
+  components: {Groups, Users}
 }
 </script>
 
