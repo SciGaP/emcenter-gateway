da686e727f5a905ffd2f1d5ca0eeedfe06e854e9
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-06-29 16:18:20 -0400

    Fix the callback route

--- src/pages/CallbackPage.vue
+++ src/pages/CallbackPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <p>Redirecting...</p>
+  <p></p>
 </template>
 
 <script>
 
-import store from "../store";
+import store from "custos-demo-gateway/src/lib/store";
+// import {custosService} from "custos-demo-gateway/src/lib/store/util/custos.util";
 
 export default {
   name: "Callback",
@@ -14,12 +15,17 @@ export default {
       let code = this.$route.query.code
       let params = {code: code};
       await this.$store.dispatch('auth/authenticateUsingCode', params)
-
+      // await this.$store.dispatch("user/addRolesToUser", {
+      //   clientId: custosService.clientId,
+      //   username: this.$store.getters["auth/currentUsername"],
+      //   realmRoles: ["tenant-requester"],
+      //   clientLevel: false
+      // });
     }
   },
   async mounted() {
     await this.authenticate();
-    await this.$router.push('/collections');
+    await this.$router.push('/');
   }
 }
 </script>
