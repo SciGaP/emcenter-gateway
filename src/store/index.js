e67b73feb78cbbcad2b0d3017eec3ba0b19c7092
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-09-07 20:06:47 -0400

    Refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
-// import user from "./modules/user.store";
-// import group from "./modules/group.store";
-// import tenant from "./modules/tenant.store";
-// import sharing from "./modules/sharing.store";
-// import entity from "./modules/entity.store";
-// import auth from "./modules/auth.store";
-// import ciLogon from "./modules/ci-logon.store";
 
-import emcFile from "./modules/emc/emc-file.store";
-import emcFolder from "./modules/emc/emc-folder.store";
+
 import emcResource from "./modules/emc/emc-resource.store";
-import emcCollectionGroup from "./modules/emc/emc-collection-group.store";
-// import emcStorageSettings from "./modules/emc/emc-storage-settings.store";
 import emcStoragePreference from "./modules/emc/emc-storage-preference.store";
 import emcStorage from "./modules/emc/emc-storage.store";
 
@@ -28,18 +18,7 @@ const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
     modules: {
-        // auth,
-        // user,
-        // group,
-        // tenant,
-        // sharing,
-        // entity,
-        // ciLogon,
-        "emcFile": emcFile,
-        "emcFolder": emcFolder,
         "emcResource": emcResource,
-        "emcCollectionGroup": emcCollectionGroup,
-        // "emcStorageSettings": emcStorageSettings,
         "emcStoragePreference": emcStoragePreference,
         "emcStorage": emcStorage
     },
